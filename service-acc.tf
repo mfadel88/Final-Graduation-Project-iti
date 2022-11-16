2dafbb16983457be44af5c4373bc1acdb63c4349
mohammed Fadel <engmohamedfadel838@gmail.com>
2022-11-16 12:36:44 +0200

    1st uplaod final to remote

---
+++ service-acc.tf
@@ -0,0 +1,14 @@
+resource "google_service_account" "final-service-acc" {
+  account_id   = "final-service-acc"
+  display_name = "Service-Account"
+  project      = "final-proj-fadel"
+}
+
+resource "google_project_iam_binding" "project" {
+  project = "final-proj-fadel"
+  role    = "roles/storage.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.final-service-acc.email}",
+  ]
+}
